f3128c86709d28ef3ab7fa1ccf30c1b62ab1076e
stephanie jones <sjones.njones@gmail.com>
2023-08-27 09:42:30 -0500

    added starter code and set up models with associations

---
+++ models/Product.js
@@ -0,0 +1,54 @@
+// import important parts of sequelize library
+const { Model, DataTypes } = require('sequelize');
+// import our database connection from config.js
+const sequelize = require('../config/connection');
+
+// Initialize Product model (table) by extending off Sequelize's Model class
+class Product extends Model { }
+
+// set up fields and rules for Product model
+Product.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.Decimal(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+        msg: "Must be an decimal number"
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      isNumeric: true,
+    },
+    // * References the `Category` model's `id`
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'category_id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product',
+  }
+);
+
+module.exports = Product;
